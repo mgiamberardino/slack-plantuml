eae6bf5685e4f5fcc8bf8d3f2291c2f800fd8940
Mauro J Giamberardino <mgiamberardino@gmail.com>
2016-07-23 17:10:52 -0300

    Port updated

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var utils = require('./utils');
 utils.initilize();
+app.get('port')
 var Routes = require('./routes/all');
 
 utils.getApp().use('/plant', Routes.getRoutes());
 
-utils.getApp().listen(5000, function() {
+utils.getApp().listen((process.env.PORT || 5000), function() {
   console.log("Node server running on http://localhost:3000");
   console.log("Connected to the database");
 });
