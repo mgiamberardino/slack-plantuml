7633d408b32b827330bbe286d059566210bf024d
Mauro J Giamberardino <mgiamberardino@gmail.com>
2016-07-23 17:58:26 -0300

    Port updated

--- controllers/plantuml.js
+++ controllers/plantuml.js
@@ -4,11 +4,19 @@ var plantumlEncoder = require('plantuml-encoder')
 var fs = require('fs'),
     request = require('request');
 
+function htmlDecode(input){
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
+
 exports.generatePng = function(req, res) {
+    console.log("Trying to generate an UML image.")
+
     plantuml.useNailgun();
 
     res.set('Content-Type', 'image/png');
-    var encoded = plantumlEncoder.encode(req.body.text)
+    var encoded = plantumlEncoder.encode(htmlDecode(req.body.text));
     console.log('http://www.plantuml.com/plantuml/img/'+encoded);
     res.status(200).jsonp({text: 'http://www.plantuml.com/plantuml/img/'+encoded})
     //request('http://www.plantuml.com/plantuml/img/'+encoded).pipe(res);
