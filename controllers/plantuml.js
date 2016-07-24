32992154e2c19c58088aa8ac03bd14c1f9092c53
Mauro J Giamberardino <mgiamberardino@gmail.com>
2016-07-24 11:14:00 -0300

    Port updated

--- controllers/plantuml.js
+++ controllers/plantuml.js
@@ -24,10 +24,10 @@ exports.generatePng = function(req, res) {
         console.log('http://www.plantuml.com/plantuml/img/'+encoded);
         res.status(200).jsonp({text: 'http://www.plantuml.com/plantuml/img/'+encoded})
       } else {
-        res.status(400).jsonp({});
+        res.status(200).jsonp({text: ""});
       }
     } else {
-      res.status(400).jsonp({});
+      res.status(200).jsonp({text: ""});
     }
     //request('http://www.plantuml.com/plantuml/img/'+encoded).pipe(res);
 };
